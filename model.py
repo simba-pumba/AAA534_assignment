0a2aae6179b3de5fad478ca8a693bd2bcad7ba1c
simba-pumba <54436635+simba-pumba@users.noreply.github.com>
2021-04-14 14:58:37 +0900

    Update model.py

--- model.py
+++ model.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 class Block(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(Block, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
 
